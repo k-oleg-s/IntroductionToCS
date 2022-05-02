b9be653f62ce494fa91b1d5716434e76642c6d3d
Олег_from_Acer <k.oleg.s@live.ru>
2022-05-02 23:27:49 +0300

    Lesson1Task1 HomeWork

---
+++ Lesson1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+
+            Console.Write("Please, enter your name: ");
+            String name = Console.ReadLine();
+            Console.WriteLine($"Привет  {name}, сегодня {today.ToString("D")}");
+            Console.ReadLine();
+        }
+    }
+}
