e8f6db90d568f53c84104542eccce31e3c10da88
OlegKS <k_olegs@mail.ru>
2022-05-16 21:56:20 +0300

    commit1

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -4,11 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson1
+namespace Lessons
 {
     internal class Program
     {
         static void Main(string[] args)
+        {
+            Lesson2Task1(); 
+            Lesson2Task1();
+            Lesson2Task1();
+            Lesson2Task1();
+            Lesson2Task1();
+            Lesson2Task1();
+        }
+
+
+        static void Lesson2Task1()
+        {
+            Console.WriteLine("введите минимальную температуру дня");
+
+        }
+
+        static void Lesson2Task2()
+        {
+            Console.WriteLine("введите минимальную температуру дня");
+            double minT = Double.Parse(Console.ReadLine());
+            Console.WriteLine("введите максимальную температуру дня");
+            double maxT = Double.Parse(Console.ReadLine());
+            Console.WriteLine($"средняя температура {(maxT + minT) / 2} ");
+
+        }
+        static void Lesson2Task3()
+        {
+            Console.WriteLine("введите минимальную температуру дня");
+
+        }
+        static void Lesson2Task4()
+        {
+            Console.WriteLine("введите минимальную температуру дня");
+
+        }
+        static void Lesson2Task5()
+        {
+            Console.WriteLine("введите минимальную температуру дня");
+
+        }
+        static void Lesson2Task6()
+        {
+            Console.WriteLine("введите минимальную температуру дня");
+
+        }
+        void Lesson1()
         {
             DateTime today = DateTime.Now;
 
